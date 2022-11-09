39ba4931e62c9885c59d01c01230fbb393fa6482
sumaya-kabir <sumayakabir4547@gmail.com>
2022-11-09 08:38:33 +0600

    firebase addes

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,11 +1,132 @@
+import { Button, Label, Select, Textarea, TextInput } from 'flowbite-react';
 import React from 'react';
 import useTitle from '../../hooks/useTitle';
+import { FaLocationArrow, FaEnvelope, FaPhoneAlt, FaCalendarAlt } from "react-icons/fa";
 
 const Contact = () => {
     useTitle('Contact')
     return (
-        <div>
-            Contact Us
+        <div className='grid grid-cols-2'>
+            <div>
+                <h2 className='text-3xl'>Contact Us</h2>
+                <form className="flex flex-col gap-4 m-5">
+                    <div>
+                        <div className="mb-2 block">
+                            <Label
+                                htmlFor="firstName"
+                                value="First Name"
+                            />
+                        </div>
+                        <TextInput
+                            id="firstName"
+                            type="text"
+                            required={true}
+                        />
+                    </div>
+                    <div>
+                        <div className="mb-2 block">
+                            <Label
+                                htmlFor="lastName"
+                                value="Last Name"
+                            />
+                        </div>
+                        <TextInput
+                            id="lastName"
+                            type="text"
+                            required={true}
+                        />
+                    </div>
+                    <div>
+                        <div className="mb-2 block">
+                            <Label
+                                htmlFor="email1"
+                                value="Your email"
+                            />
+                        </div>
+                        <TextInput
+                            id="email1"
+                            type="email"
+                            placeholder="name@flowbite.com"
+                            required={true}
+                        />
+                    </div>
+                    <div id="select">
+                        <div className="mb-2 block">
+                            <Label
+                                htmlFor="countries"
+                                value="Select your country"
+                            />
+                        </div>
+                        <Select
+                            id="countries"
+                            required={true}
+                        >
+                            <option>
+                                United States
+                            </option>
+                            <option>
+                                Canada
+                            </option>
+                            <option>
+                                France
+                            </option>
+                            <option>
+                                Germany
+                            </option>
+                            <option>
+                                Bangladesh
+                            </option>
+                            <option>
+                                India
+                            </option>
+                        </Select>
+                    </div>
+                    <div id="textarea">
+                        <div className="mb-2 block">
+                            <Label
+                                htmlFor="comment"
+                                value="Your message"
+                            />
+                        </div>
+                        <Textarea
+                            id="comment"
+                            placeholder="Leave a comment..."
+                            required={true}
+                            rows={4}
+                        />
+                    </div>
+                    <Button type="submit" gradientDuoTone="pinkToOrange">
+                        Submit
+                    </Button>
+                </form>
+            </div>
+            <div className='mx-10'>
+                <h2 className='text-3xl mb-5'>Our Office</h2>
+                <div className='w-full'>
+                    <div className="flex">
+                    <FaLocationArrow className='text-orange-500 mt-2 mr-3'></FaLocationArrow>
+                    <p className='text-xl pb-5'> Edmonton City, Alberta, Canada</p>
+                    </div>
+                    <div className="flex">
+                    <FaEnvelope
+                    className='text-orange-500 mt-2 mr-3'></FaEnvelope>
+                    <p className='text-xl pb-5'> getvisa@gmail.com</p>
+                    </div>
+                    <div className="flex">
+                    <FaPhoneAlt
+                    className='text-orange-500 mt-2 mr-3'></FaPhoneAlt>
+                    <p className='text-xl pb-5'> +234567898765</p>
+                    </div>
+                    <div className="flex">
+                    <FaCalendarAlt
+                    className='text-orange-500 mt-2 mr-3'></FaCalendarAlt>
+                    <div>
+                        <p className='text-xl'> Opening hours</p>
+                        <p className='text-xl'> 9 am - 5 pm</p>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Banner/Slider.js
+++ src/Pages/Home/Banner/Slider.js
@@ -5,26 +5,51 @@ const Slider = () => {
     return (
         <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
             <Carousel slideInterval={5000}>
-                <img
-                    src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-                    alt="..."
-                />
-                <img
-                    src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-                    alt="..."
-                />
-                <img
-                    src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-                    alt="..."
-                />
-                <img
-                    src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-                    alt="..."
-                />
-                <img
-                    src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-                    alt="..."
-                />
+
+                <div class="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-0 z-20" data-carousel-item="">
+                    <img src="https://images.pexels.com/photos/7888671/pexels-photo-7888671.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="block  absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt=""/>
+                        <div class="relative top-500 top-1/2 px-4 py-2 opacity-100 ">
+                            <h6 className="text-xl text-white font-bold text-center ">Welcome to GET VISA</h6>
+                            <h3 className="text-5xl text-white font-bold text-center ">Immegration And Visa Consultant Agent</h3>
+
+                        </div>
+                </div>
+
+                <div class="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-0 z-20" data-carousel-item="">
+                    <img src="https://images.pexels.com/photos/7889237/pexels-photo-7889237.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="block  absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt=""/>
+                        <div class="relative top-500 top-1/2 px-4 py-4 opacity-100">
+                            <h6 className="text-xl text-white font-bold text-center ">Welcome to GET VISA</h6>
+                            <h3 className="text-5xl text-white font-bold text-center ">Immegration And Visa Consultant Agent</h3>
+
+                        </div>
+                </div>
+
+                <div class="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-0 z-20" data-carousel-item="">
+                    <img src="https://images.pexels.com/photos/7888650/pexels-photo-7888650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="block  absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt=""/>
+                        <div class="relative top-500 top-1/2 px-4 py-4 opacity-100">
+                            <h6 className="text-xl text-white font-bold text-center ">Welcome to GET VISA</h6>
+                            <h3 className="text-5xl text-white font-bold text-center ">Immegration And Visa Consultant Agent</h3>
+
+                        </div>
+                </div>
+
+                <div class="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-0 z-20" data-carousel-item="">
+                    <img src="https://images.pexels.com/photos/6248947/pexels-photo-6248947.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="block  absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt=""/>
+                        <div class="relative top-500 top-1/2 px-4 py-2 opacity-100 ">
+                            <h6 className="text-xl text-white font-bold text-center ">Welcome to GET VISA</h6>
+                            <h3 className="text-5xl text-white font-bold text-center ">Immegration And Visa Consultant Agent</h3>
+
+                        </div>
+                </div>
+                
+                <div class="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-0 z-20" data-carousel-item="">
+                    <img src="https://images.pexels.com/photos/6248978/pexels-photo-6248978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="block  absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt=""/>
+                        <div class="relative top-500 top-1/2 px-4 py-2 opacity-100 ">
+                            <h6 className="text-xl text-white font-bold text-center ">Welcome to GET VISA</h6>
+                            <h3 className="text-5xl text-white font-bold text-center ">Immegration And Visa Consultant Agent</h3>
+
+                        </div>
+                </div>
             </Carousel>
         </div>
     );
