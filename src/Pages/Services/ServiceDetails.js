205d967cb326b59c6a023f64d339e07a7c5b3f58
sumaya-kabir <sumayakabir4547@gmail.com>
2022-11-10 11:53:24 +0600

    review section

--- src/Pages/Services/ServiceDetails.js
+++ src/Pages/Services/ServiceDetails.js
@@ -1,14 +1,41 @@
+import { Card } from 'flowbite-react';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
+import Reviews from '../Reviews/Reviews';
 
 const ServiceDetails = () => {
-    const {title, picture, description, price} = useLoaderData();
+    const { title, picture, description, price, reviews } = useLoaderData();
     return (
-        <div>
-            <h2 className='text-4xl font-semibold'>{title}</h2>
-            <img src={picture} alt="" />
-            <p>{description}</p>
-            <p className='text-xl text-orange-500'>Price: <span className='font-medium'> {price}$</span></p>
+        <div className='grid grid-cols-1 lg:grid-cols-2'>
+            <div className='m-6'>
+                <h2 className='text-4xl font-semibold mb-5'>{title}</h2>
+
+                <PhotoProvider>
+                        <PhotoView src={picture}>
+                            <img src={picture} alt="" />
+                        </PhotoView>
+                    </PhotoProvider>
+                
+                <p className='mt-5 font-normal'>{description}</p>
+                <p className='border border-orange-500 text-center text-xl text-orange-500 mt-5'>Service Charge: <span className='font-medium'> {price}$</span></p>
+            </div>
+            <div>
+                <h2 className='text-2xl text-center'>Our Client Reviews</h2>
+
+                <div className="">
+                    <Card className='m-6'>
+                        <div>
+                            {
+                                reviews.map(review => <Reviews
+                                review={review}
+                                ></Reviews>)
+                            }
+                        </div>
+                    </Card>
+                </div>
+            </div>
         </div>
     );
 };
