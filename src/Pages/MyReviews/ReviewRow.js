5dcc7e0a9fe479bc61d6720d23ecd0632d29612d
sumaya-kabir <sumayakabir4547@gmail.com>
2022-11-11 23:09:11 +0600

    review row modified

--- src/Pages/MyReviews/ReviewRow.js
+++ src/Pages/MyReviews/ReviewRow.js
@@ -1,16 +1,16 @@
 import { Table } from 'flowbite-react';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const ReviewRow = ({review, handleDelete, handleStatusUpdate}) => {
-    const {_id, serviceId, serviceName, reviewText} = review;
-    const [reviewService, setReviewService] = useState({});
+    const {_id, serviceName, reviewText} = review;
+    // const [reviewService, setReviewService] = useState({});
 
 
-    useEffect( () => {
-        fetch(`https://getvisa-server.vercel.app/reviews/${serviceId}`)
-        .then(res => res.json())
-        .then( data => setReviewService(data))
-    }, [serviceId])
+    // useEffect( () => {
+    //     fetch(`https://getvisa-server.vercel.app/reviews/${serviceId}`)
+    //     .then(res => res.json())
+    //     .then( data => setReviewService(data))
+    // }, [serviceId])
     return (
             <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
                         <Table.Cell className="!p-4">
